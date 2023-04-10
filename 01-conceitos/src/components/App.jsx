065eee7c886ab4bb53dcffa26f3034a36efd904b
João Lucas <jlmachado4004@gmail.com>
2023-04-10 12:04:44 -0400

    Developing 1-module

---
+++ 01-conceitos/src/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Header } from './Header'
+
+import './global.css';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
